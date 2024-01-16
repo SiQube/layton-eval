9b69daac544c354936c2d163676444e1a451e86a
Raphael Vienne <raphael.vienne@live.fr>
2024-01-16 08:39:37 +0100

    add answer image scraping*

--- src/layton_eval/get_eng_html.py
+++ src/layton_eval/get_eng_html.py
@@ -26,13 +26,19 @@ def get_all_links(url: str) -> t.List[str]:
     return links, next_button
 
 
-def get_puzzle_image(soup: BeautifulSoup) -> Image:
+def get_puzzle_images(soup: BeautifulSoup) -> Image:
     """
-    Returns the game frame associated to a puzzle.
+    Returns the game frame associated to a puzzle and its answer (when applicable).
     """
     if soup.select_one(".image.image-thumbnail") is not None:
         img = Image.open(BytesIO(requests.get(soup.select_one(".image.image-thumbnail").attrs["href"]).content))
-        return img
+        answer_img = soup.select_one(f"""[alt="{soup.select_one(".image.image-thumbnail").attrs["title"] + "S"}"]""")
+        if answer_img is not None:
+            link = answer_img.attrs.get("data-src", answer_img.attrs.get("src"))
+            answer_img = Image.open(BytesIO(requests.get(link).content))
+        return img, answer_img
+    else:
+        return None, None
 
 
 if __name__ == "__main__":
@@ -51,12 +57,13 @@ if __name__ == "__main__":
     os.makedirs(f"{ROOT_DIR}/layton-data", exist_ok=True)
     os.makedirs(f"{ROOT_DIR}/layton-data/htmls", exist_ok=True)
     os.makedirs(f"{ROOT_DIR}/layton-data/images", exist_ok=True)
+    os.makedirs(f"{ROOT_DIR}/layton-data/answer_images", exist_ok=True)
     for url in tqdm(riddle_urls):
         response = requests.get(url)
         if response.status_code == 200:
             content = response.content
             soup = BeautifulSoup(content, 'html.parser')
-            img = get_puzzle_image(soup)
+            img, answer_img = get_puzzle_images(soup)
             puzzle_name = url.split("Puzzle:")[-1].replace("/", "_")
             with open(f"{ROOT_DIR}/layton-data/htmls/{puzzle_name}.html", "wb") as f:
                 f.write(content)
@@ -64,5 +71,9 @@ if __name__ == "__main__":
                 img.convert("RGB").save(f"{ROOT_DIR}/layton-data/images/{puzzle_name}.jpg")
             else:
                 log.info(f"No img found for puzzle at: {url}")
+            if answer_img is not None:
+                answer_img.convert("RGB").save(f"{ROOT_DIR}/layton-data/answer_images/{puzzle_name}.jpg")
+            else:
+                log.info(f"No answer_img found for puzzle at: {url}")
         else:
             log.info(f"Encountered error while navigating to {url}")
